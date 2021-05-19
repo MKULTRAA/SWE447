e7bb3c3f52b1e15c43812726cc6d1ff4eb4ce58a
Samy Du <33183498+MKULTRAA@users.noreply.github.com>
2021-05-18 20:59:12 -0700

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -1,22 +1,27 @@
 var gl = null;
+var cone = null;
 
 function init() {
-    var canvas = document.getElementById( "webgl-canvas" );
+    var canvas = document.getElementById("webgl-canvas");
 
-    gl = WebGLUtils.setupWebGL( canvas );
+    gl = WebGLUtils.setupWebGL(canvas);
 
-    if ( !gl ) {
+    if (!gl) {
         alert("Unable to setup WebGL");
         return;
     }
 
-    gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
+    cone = new Cone(gl, n);
+
+    gl.clearColor(1.0, 0.0, 1.0, 1.0);
 
     render();
 }
 
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    cone.render();
 }
 
 window.onload = init;
