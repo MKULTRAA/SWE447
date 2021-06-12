bd413e54f558affb668f84c260207033fd1b06df
Samy Du <33183498+MKULTRAA@users.noreply.github.com>
2021-06-11 17:17:37 -0700

    Update Solar.js

--- Assignment-5/Solar.js
+++ Assignment-5/Solar.js
@@ -11,23 +11,23 @@ var gl;
 //
 //  Declare our array of planets (each of which is a sphere)
 //
-// The list of planets to render.  Uncomment any planets that you are 
-// including in the scene. For each planet in this list, make sure to 
+// The list of planets to render.  Uncomment any planets that you are
+// including in the scene. For each planet in this list, make sure to
 // set its distance from the Sun, as well its size, color, and orbit
-// around the Sun. 
+// around the Sun.
 
 var Planets = {
   Sun : undefined,
   Mercury : undefined,
   Venus : undefined,
   Earth : undefined,
-  Moon : undefined,
-  Mars : undefined,
-  Jupiter : undefined,
-  Saturn : undefined,
-  Uranus : undefined,
-  Neptune : undefined,
-  Pluto : undefined
+   Moon : undefined,
+   Mars : undefined,
+   Jupiter : undefined,
+   Saturn : undefined,
+   Uranus : undefined,
+   Neptune : undefined,
+   Pluto : undefined
 };
 
 // Viewing transformation parameters
@@ -35,11 +35,13 @@ var V;  // matrix storing the viewing transformation
 
 // Projection transformation parameters
 var P;  // matrix storing the projection transformation
-var near = 10;      // near clipping plane's distance
-var far = 120;      // far clipping plane's distance
+//var near = 10;      // near clipping plane's distance
+//var far = 120;      // far clipping plane's distance
+var near = 1;      // near clipping plane's distance
+var far = 200;      // far clipping plane's distance
 
 // Animation variables
-var time = 0.0;      // time, our global time constant, which is 
+var time = 0.0;      // time, our global time constant, which is
                      // incremented every frame
 var timeDelta = 0.5; // the amount that time is updated each fraime
 
@@ -71,12 +73,12 @@ function init() {
 
     var planet = Planets[name] = new Sphere();
 
-    // For each planet, we'll add a new property (which itself is a 
+    // For each planet, we'll add a new property (which itself is a
     // dictionary) that contains the uniforms that we will use in
     // the associated shader programs for drawing the planets.  These
     // uniform's values will be set each frame in render().
 
-    planet.uniforms = { 
+    planet.uniforms = {
       color : gl.getUniformLocation(planet.program, "color"),
       MV : gl.getUniformLocation(planet.program, "MV"),
       P : gl.getUniformLocation(planet.program, "P"),
@@ -85,7 +87,7 @@ function init() {
 
   resize();
 
-  window.requestAnimationFrame(render);  
+  window.requestAnimationFrame(render);
 }
 
 //---------------------------------------------------------------------------
@@ -100,11 +102,11 @@ function render() {
 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  // Specify the viewing transformation, and use it to initialize the 
+  // Specify the viewing transformation, and use it to initialize the
   // matrix stack
 
   V = translate(0.0, 0.0, -0.5*(near + far));
-  ms.load(V);  
+  ms.load(V);
 
   // Create a few temporary variables to make it simpler to work with
   // the various properties we'll use to render the planets.  The Planets
@@ -120,7 +122,7 @@ function render() {
   name = "Sun";
   planet = Planets[name];
   data = SolarSystem[name];
-  
+
   // Set PointMode to true to render all the vertices as points, as
   // compared to filled triangles.  This can be useful if you think
   // your planet might be inside another planet or the Sun.  Since the
@@ -145,7 +147,7 @@ function render() {
 
   //
   //  Add your code for more planets here!
-  //
+
  
   name = "Mercury";
   planet = Planets[name];
@@ -210,7 +212,7 @@ function render() {
   ms.pop();
 
 
- 
+  
   name = "Moon";
   planet = Planets[name];
   data = SolarSystem[name];
@@ -310,7 +312,7 @@ function render() {
   planet.render();
   ms.pop();
 
- 
+  
   name = "Neptune";
   planet = Planets[name];
   data = SolarSystem[name];
@@ -350,6 +352,7 @@ function render() {
   planet.render();
   ms.pop();
   //
+
   window.requestAnimationFrame(render);
 }
 
@@ -364,7 +367,7 @@ function resize() {
 
   gl.viewport(0, 0, w, h);
 
-  var fovy = 100.0; // degrees
+  var fovy = 60.0; // degrees
   var aspect = w / h;
 
   P = perspective(fovy, aspect, near, far);
