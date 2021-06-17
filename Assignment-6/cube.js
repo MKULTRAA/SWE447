d0d96a2eb9d255b0660d29a77ca3655a75bcba30
MKULTRA <33183498+MKULTRAA@users.noreply.github.com>
2021-06-17 07:54:47 -0700

    Update cube.js

--- Assignment-6/cube.js
+++ Assignment-6/cube.js
@@ -22,6 +22,30 @@ function init() {
   render();
   }
   
+function initTexture () {
+  texture = gl.createTexture();
+  texImage = new Image();
+  texImage.onload = function () {
+     loadTexture(image, texture);
+  };
+  texImage.src = "https://i.imgur.com/w9NORXB.jpg";
+}
+
+function loadTexture(image, texture) {
+gl.bindTexture(gl.TEXTURE_2D, texture);
+ gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
+gl.RGBA, gl.UNSIGNED_BYTE, image);
+ gl.texParameteri(gl.TEXTURE_2D,  
+gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+ gl.texParameteri(gl.TEXTURE_2D,
+gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+ gl.generateMipmap(gl.TEXTURE_2D);
+ gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+
+
+
                          
 
 
